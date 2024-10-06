24d2f41b31e122edf45c47872f1bb032cd583547
melad <meladzeruslan164@gmail.com>
2024-10-06 19:48:39 +0300

    repetition of functions

--- basic.py
+++ basic.py
@@ -6,9 +6,20 @@
 
 # print('Leon', 100)
 
-name = "Leon"
-number = 100
+# name = "Leon"
+# number = 100
 
-result = f'{name} - {number}'
+# result = f'{name} - {number}'
 
-print(result)
+# print(result)
+
+
+greeting = 'Hello'
+to = 'World'
+
+# Welcome greeting function
+def greet(message, name="World"):
+    result = f"{message}, {name}"
+    print(result)
+
+greet(greeting, to)
