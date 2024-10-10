39e5601b89ae14d5307424890773f445b59197cb
melad <meladzeruslan164@gmail.com>
2024-10-11 00:52:01 +0300

    lists, sorted

--- lists.py
+++ lists.py
@@ -1,5 +1,12 @@
 
 persons = ['kravchuk_1990', 'pavlov_1993', 'fedik_1994', 'Rapota_1994']
 
-s_persons = sorted(persons)
+# s_persons = sorted(persons)
+# print(s_persons)
+
+# function for sorted
+def by_year(name):
+    return name.split('_')[-1]
+
+s_persons = sorted(persons, key=by_year)
 print(s_persons)
