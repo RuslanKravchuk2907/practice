883a4ca523fef447086264f2c66cd2ea46ff9389
melad <meladzeruslan164@gmail.com>
2024-10-13 23:34:04 +0300

    dictionaries, type None, functions lvl 2

---
+++ dict_None_functions_2.py
@@ -0,0 +1,16 @@
+
+person = {'name': 'ruslan', 'surname': 'kravchuk', 'email': 'test@gmail.com'}
+
+d = dict(name='roman', tel='12345678')
+
+new = {'name': 'dima', 'tel': '17876862'}
+
+person.update(name='romka', tel='10987203')
+
+
+def a():
+    print('Hello world')
+    return
+
+result = a()
+print(result)
