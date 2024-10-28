6d42a0514cc705f599a11f2c59f943e7d3e573ee
melad <meladzeruslan164@gmail.com>
2024-10-28 17:12:02 +0200

    practice

--- MA2.py
+++ MA2.py
@@ -1,7 +1,19 @@
-def add_numbers(a: int, b: int) -> int:
-    print(f"Adding {type(a)} and {type(b)}")
-    return a + b
+import pdb
+
+# def add_numbers(a: int, b: int) -> int:
+#     print(f"Adding {type(a)} and {type(b)}")
+#     return a + b
+#
+#
+# add_numbers(2, "3")
 
 
-add_numbers(2, "3")
+numbers = [1, "2", 3]
 
+my_sum = 0
+
+for number in numbers:
+    pdb.set_trace()
+    my_sum += number
+
+    print(my_sum)
