503e1b19cf3ca42323b9f173a8b7d85ad3baf126
melad <meladzeruslan164@gmail.com>
2024-10-30 02:32:30 +0200

    practice

--- MA2.py
+++ MA2.py
@@ -1,17 +1,52 @@
-def find_max(numbers):
-    if not numbers:
-        return None  # Повертаємо None, якщо список порожній
+# def calculate_average(numbers):
+#     total = 0
+#     count = len(numbers)
+#     for number in numbers:
+#         total += number
+#
+#     if count > 0:
+#         return total / count
+#
+#     return 0
 
-    max_num = numbers[0]  # Ініціалізуємо max_num першим елементом списку
-    for num in numbers:
-        if num > max_num:
-            max_num = num
-    return max_num
+
+# user_input = "   username@example.com   "
+#
+# #  Write code here
+# cleaned_input = user_input.strip()
+# left_strip_input = user_input.lstrip()
+# right_strip_input = user_input.rstrip()
+#
+#
+# print(user_input)
+# print(cleaned_input )
+# print(left_strip_input)
+# print(right_strip_input)
 
 
-# Тести функції
-print(find_max([3, 5, 7, 2, 8]))  # Виведе: 8
-print(find_max([-10, -5, -1, -20]))  # Виведе: -1
-print(find_max([42]))  # Виведе: 42
-print(find_max([]))  # Виведе: None
+# def count_substring(sentence: str, substring: str) -> int:
+#     #  Write code here
+#     count = sentence.count(substring)
+#
+#
+# print(count_substring("Thank you for coming! Thank you for being with us!", "Thank you"))
+#
+# print(count_substring("I love my job because it gives me satisfaction", "love"))
+#
+#
+# def weekday_order(weekday: str) -> int:
+#     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+#     return days.index(weekday)
+#
+# def sort_weekdays(weekdays: list) -> list:
+#     return sorted(weekdays, key=weekday_order)
 
+
+african_animals = ["Lion", "Leopard", "Elephant", "Rhinoceros", "Giraffe"]
+new_african_animals = ["Hippo", "Gorilla"]
+european_animals = ["Brown Bear", "Wild Boar", "Polar Bear", "Red Deer"]
+# write your code below
+animals = african_animals + new_african_animals
+animals.extend(european_animals)
+
+print(animals)
